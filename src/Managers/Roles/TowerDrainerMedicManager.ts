9b68c768999135e8f369405d2c44448b0aa880eb
Jake <jcwaldrip@att.net>
2019-08-02 08:37:22 -0500

    added function definitions for tower drainers, filled out a couple

--- src/Managers/Roles/TowerDrainerMedicManager.ts
+++ src/Managers/Roles/TowerDrainerMedicManager.ts
@@ -24,4 +24,45 @@ export default class TowerDrainerMedicCreepManager implements ICreepRoleManager 
         const CREEP_RANGE: number = 1;
 
     }
+
+    /**
+     * Check creep basics for tower medic
+     * @param creep the tower medic creep
+     */
+    private checkTowerMedicBasics(creep: Creep): void {
+
+    }
+
+    /**
+     * Rally the creep with the tower tank
+     * @param creep the tower medic creep
+     */
+    private rallyWithTank(creep: Creep): void {
+
+    }
+
+    /**
+     * Get the tower tanks target position
+     * @param creep the tower medic creep
+     */
+    private getTargetPosition(creep: Creep): void {
+
+    }
+
+    /**
+     * Get the tower tank in your squad
+     * @param creep the tower medic creep
+     */
+    private getTowerTankInSquad(creep: Creep): Creep[] {
+        return [creep];
+    }
+
+    /**
+     * Move in formation with the tower tank
+     * @param creep the tower medic creep
+     * @param towerTank the tower tank
+     */
+    private moveInFormation(creep: Creep): void {
+
+    }
 }

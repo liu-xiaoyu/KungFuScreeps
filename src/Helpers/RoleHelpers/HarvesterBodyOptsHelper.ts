733cefb5171de0f423265b2444576e252db8c162
Jake <jcwaldrip@att.net>
2019-11-14 17:30:45 -0600

    fixed something that was killing my room. please don't look

--- src/Helpers/RoleHelpers/HarvesterBodyOptsHelper.ts
+++ src/Helpers/RoleHelpers/HarvesterBodyOptsHelper.ts
@@ -148,6 +148,7 @@ export class HarvesterBodyOptsHelper implements ICreepBodyOptsHelper {
                     getLootJobs: true,
                     getFromStorage: true, //
                     getDroppedEnergy: true, //
+                    getFromContainer: true
                 };
 
                 break;

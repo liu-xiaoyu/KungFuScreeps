6a1b4e7c299d218bf16260c00a73c3f91f4e0b3e
Jake <jcwaldrip@att.net>
2019-11-22 09:40:38 -0600

    made remote harvesters bigger

--- src/Helpers/RoleHelpers/RemoteHarvesterBodyOptsHelper.ts
+++ src/Helpers/RoleHelpers/RemoteHarvesterBodyOptsHelper.ts
@@ -54,8 +54,8 @@ export class RemoteHarvesterBodyOptsHelper implements ICreepBodyOptsHelper {
                 break;
 
             case TIER_8:
-            case TIER_7: // Total Cost: 1900
-                body = { work: 2, carry: 16, move: 18 };
+            case TIER_7: // Total Cost: 2200
+                body = { work: 2, carry: 18, move: 20 };
                 break;
         }
 
